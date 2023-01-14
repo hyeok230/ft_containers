d405492d1fe8abda3e44d5dda94b9f0d18dc537d
hyeok230 <hyeok230@naver.com>
2023-01-14 20:16:05 +0900

    add: stack member type

--- stack.hpp
+++ stack.hpp
@@ -5,7 +5,17 @@
 
 namespace ft 
 {
-    
+    template<class T, class Container = std::vector<T> >
+    class stack
+    {
+        public:
+            typedef Container                   container_type;
+            typedef typename container_type::value_tpye  value_type;
+            typedef typename container_type::size_type   size_type;
+            
+
+
+    };
 
 
 
