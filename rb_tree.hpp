2334f154523ebab4cfa30b30b3caef6cb0e16a82
hyeok230 <hyeok230@naver.com>
2023-01-28 21:30:41 +0900

    feat: rb_tree start

---
+++ rb_tree.hpp
@@ -0,0 +1,23 @@
+#ifndef RB_TREE_HPP
+#define RB_TREE_HPP
+
+#include <memory>
+#include <functional>
+#include <cstddef>
+#include "reverse_iterator.hpp"
+#include "algorithm.hpp"
+#include "pair.hpp"
+
+namespace ft
+{
+    enum color { black = 0, red = 1};
+
+    template <typename T>
+	class rb_tree_node
+	{
+        
+
+    };
+}
+
+#endif
